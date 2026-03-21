14a3fb78efb456a6c18bb52a00d913a0c1e0090d
Eric Linnnnnn <a060620100@gmail.com>
2026-03-21 14:27:10 +0800

    Add files via upload

--- text.py
+++ text.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine ,text
 import plotly.express as px
 import plotly.graph_objects as go
 
@@ -9,12 +9,14 @@ import plotly.graph_objects as go
 st.set_page_config(page_title="經濟健康度儀表板", layout="wide")
 
 BASE_URL = "http://127.0.0.1:8000"  # FastAPI 位址
-DATABASE_URL = "mysql+pymysql://root:1qaz2WSX@8.229.26.9:3306/mydb"
+DATABASE_URL = "mysql+pymysql://Lin_Po_Wei:1qaz2WSX@8.229.26.9:3306/mydb"
 engine = create_engine(DATABASE_URL)
 
 try:
     with engine.connect() as conn:
-        result = conn.execute("SELECT NOW();")
-        st.success(f"✅ MariaDB 連線成功！資料庫時間：{result.fetchone()[0]}")
+        result = conn.execute(text("SELECT NOW();"))
+        print("✅ 連線成功！資料庫時間：", result.fetchone())
 except Exception as e:
-    st.error(f"❌ MariaDB 連線失敗：{e}")
+    print("❌ 連線失敗：", e)
+
+
